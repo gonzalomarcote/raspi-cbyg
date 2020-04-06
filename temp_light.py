7df5df0bd076e944e1b90e8fa662cb0b57c80435
Gonzalo Marcote <gonzalo.marcote@accenture.com>
2020-04-06 14:02:07 +0200

    Added new LCD `Adafruit-CharLCD` library

--- temp_light.py
+++ temp_light.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Import libraries
-from RPLCD.gpio import CharLCD
+import Adafruit_CharLCD as LCD
 from gpiozero import LED
 from gpiozero import MotionSensor, LED
 from gpiozero import MCP3008
@@ -13,6 +13,21 @@ from time import sleep
 pir = MotionSensor(4)
 light = LED(16)
 #adc = MCP3008(channel=0)
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 18
+lcd_d7 = 22
+lcd_backlight = 2
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows = 2
+
+# Initialize LCD
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
 
 # Functions
 def convert_temp(gen):
@@ -20,6 +35,7 @@ def convert_temp(gen):
         yield (value * 3.3 - 0.5) * 100
 
 # Program
+lcd.message('Hello\nworld!')
 #while True:
 pir.when_motion = light.on
 pir.when_no_motion = light.off
