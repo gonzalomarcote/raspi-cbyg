01f01bcd5ba81287aac640f0a24fdb957301f716
Gonzalo Marcote <gonzalo.marcote@accenture.com>
2020-04-17 12:27:04 +0200

    Added external temperature support with MCP3008

--- temp_light.py
+++ temp_light.py
@@ -12,7 +12,7 @@ from time import sleep
 # Variables
 pir = MotionSensor(4)
 light = LED(16)
-#adc = MCP3008(channel=0)
+adc = MCP3008(channel=0)
 lcd_rs = 25
 lcd_en = 24
 lcd_d4 = 23
@@ -30,33 +30,34 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_c
 
 
 # Functions
-# Convert sensor temperature to celsius
-def convert_temp(gen):
+# Get external Rack temperature
+def rack_temp(gen):
     for value in gen:
         yield (value * 3.3 - 0.5) * 100
 
-# Get internal Raspberry CPU temperature
-def cputemp():
+# Get internal Bastion temperature
+def bastion_temp():
     f = open("/sys/class/thermal/thermal_zone0/temp")
     CPUTemp = f.read()
     f.close()
     btemp = int(CPUTemp)/1000
     return btemp
 
+# Get internal Plex temperature
 
+# Program
 while True:
-
-    # Check temp
-    btemp = cputemp()
-    print('The raspberry pi temperature is: ' + str(btemp) + 'C')
-    #rtemp = convert_temp(adc.values)
-    #print('The external rack temperature is: ' + str(rtemp) + 'C')
 
     # Turn on/off led and lcd on pir motion for 30 seconds
     if pir.motion_detected:
         print('Door open')
         light.on()
-        lcd.message('  CBYG - RACK\n' + 'R:35  B:' + str(int(round(btemp))) +  '  P:37')
+        # Check temp
+        btemp = bastion_temp()
+        print('The raspberry pi temperature is: ' + str(btemp) + 'C')
+        rtemp = rack_temp(adc.values)
+        print('The external rack temperature is: ' + str(rtemp) + 'C')
+        lcd.message('  CBYG - RACK\n' + 'R:' + str(int(round(rtemp))) + ' B:' + str(int(round(btemp))) +  '  P:37')
         sleep(30)
     else:
         print('Door closed')
